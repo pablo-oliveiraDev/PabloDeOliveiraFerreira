985fc1cfbd00f757a9bf682684af1df5da2b791a
pablo oliveira <pabloliverfe@gmail.com>
2023-04-11 22:59:10 -0300

    erro index

--- next.config.js
+++ next.config.js
@@ -5,9 +5,15 @@ module.exports = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 }
 const nextConfig = {
   reactStrictMode: true,
 }
+
 
 module.exports = nextConfig
